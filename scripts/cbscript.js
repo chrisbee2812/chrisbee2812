04eb5cc6e7b66d68bcdd561977ce8ebd4eb8ad75
Chris Bicknell <chrisb2812@gmail.com>
2025-02-14 14:33:47 +0100

    moved aboutme and devpath text to index

--- scripts/cbscript.js
+++ scripts/cbscript.js
@@ -1,13 +1,35 @@
 
 function btn_about_me_bottom_text() {
-    if (document.getElementById("div_to_hide").style.display === 'none') {
-        document.getElementById("btn-about-me-bottom").innerText = "READ LESS";
-        
-    } else {
-        document.getElementById("btn-about-me-bottom").innerText = "READ MORE";
+    
+    if (textAboutMeIsShowing === false) {
+        document.getElementById("btn-about-me-bottom").innerHTML = 'READ LESS';
+        document.getElementById("btn-about-me-top").style.display = 'inline-block';
+        document.getElementById("div_aboutMe_to_hide").style.display = 'block';    
+        textAboutMeIsShowing = true;    
+    } else if (textAboutMeIsShowing === true) {
+        document.getElementById("btn-about-me-bottom").innerHTML = 'READ MORE';
+        document.getElementById("btn-about-me-top").style.display = 'none';
+        document.getElementById("div_aboutMe_to_hide").style.display = 'none';
+        textAboutMeIsShowing = false;
     }
 }
 
+function btn_professional_text() {
+    
+    if (textProfessionalIsShowing === false) {
+        document.getElementById("btn-professional-bottom").innerHTML = 'READ LESS';
+        document.getElementById("btn-professional-top").style.display = 'inline-block';
+        document.getElementById("div_professional_to_hide").style.display = 'block';    
+        textProfessionalIsShowing = true;    
+    } else if (textProfessionalIsShowing === true) {
+        document.getElementById("btn-professional-bottom").innerHTML = 'READ MORE';
+        document.getElementById("btn-professional-top").style.display = 'none';
+        document.getElementById("div_professional_to_hide").style.display = 'none';
+        textProfessionalIsShowing = false;
+    }
+}
 
-document.querySelector("btn-about-me-bottom").addEventListener("click", btn_about_me_bottom_text);
+var textAboutMeIsShowing = false;
+var textProfessionalIsShowing = false;
+
 
